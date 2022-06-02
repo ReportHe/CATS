e38757b2f7c20d4c5877d25bc90bfbe23f982dd2
task <2382473729@qq.com>
2022-06-02 14:38:21 +0800

    sdasd

--- Client/src/pack/PackageAstoCAuth.java
+++ Client/src/pack/PackageAstoCAuth.java
@@ -3,7 +3,9 @@ package pack;
 import struct.ArrayLengthMarker;
 import struct.StructClass;
 import struct.StructField;
-
+/*
+dasda
+ */
 @StructClass
 public class PackageAstoCAuth {
     @StructField(order = 0)
